e5761c3605fd3d0c620fbffeafc55c749f692828
Andreas Eismann <abulvenz@gmail.com>
2018-11-29 23:43:45 +0100

    Added slider for Home

--- src/main.js
+++ src/main.js
@@ -1,126 +1,174 @@
-import tagl_hyperscript from 'tagl-mithril';
-import m from 'mithril';
+import tagl_hyperscript from "tagl-mithril";
+import m from "mithril";
 
-import image_ from '../images/architect-architecture-build-1109541.jpg';
+import images from "../images/*.*";
+
+import carousel from "bulma-carousel";
+
+const images_mapped = Object.keys(images)
+  .map(key =>
+    Object.keys(images[key]).map(key2 => {
+      return { [key + "." + key2]: images[key][key2] };
+    })
+  )
+  .reduce((a, b) => a.concat(b))
+  .reduce((a, b) => Object.assign(a, b));
 
 const {
-    div,    
-    section,
-    nav,
-    footer,  
-    a  ,
-    p,
-    strong,
-    img,
-    h1,h2,h3,h4,h5,h6
+  div,
+  section,
+  nav,
+  footer,
+  a,
+  p,
+  strong,
+  img,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  span,
+  i
 } = tagl_hyperscript(m);
 
-
 class ExamplePage {
-    view(vnode) {
-        return section.section.fade(h1.title('Example'))
-    }
+  view(vnode) {
+    return section.section.fade(h1.title("Example"));
+  }
+}
+
+class Carousel {
+  oncreate(vnode) {
+    this.images = Object.keys(images_mapped);
+    this.active = 0;
+     this.bc = new carousel(vnode.dom, {autoplay:true});
+  }  
+  view(vnode) {
+    return div.carousel.carouselAnimated.carouselAnimateSlide(
+      div.carouselContainer(
+        Object.keys(images_mapped).map((img_src, idx) =>
+          div.carouselItem.hasBackground[
+            idx === this.active ? "is-active" : ""
+          ](
+            img.isBackground({
+              width: "640",
+              height: 480,
+              src: images_mapped[img_src]
+            })
+          )
+        )
+      ),
+      div.carouselNavigation.isOverlay(
+        div.carouselNavLeft(span.mdi.mdiChevronLeft.isLarge())
+      ),
+      div.carouselNavigation.isOverlay(
+        div.carouselNavRight(span.mdi.mdiChevronRight())
+      )
+    );
+  }
 }
 
 class Home {
-    view(vnode) {
-        return section.section.fade(
-            h1.title('Willkommen'),
-            p.text(`Wir freuen uns, Ihnen hier Informationen zum
+  view(vnode) {
+    return section.section.fade(
+      div.container(
+        h1.title("Willkommen"),
+        p.text(`Wir freuen uns, Ihnen hier Informationen zum
                 ökologischen Bauen, Sanieren und Umbauen zur
                 Verfügung zu stellen. 
             `),
-            img.image({
-                src:image_
-            })
-        )
-    }
+        m(Carousel,{images: images_mapped}),        
+        p.text(`Bitte stöbern Sie und zögern nicht uns auch
+            persönlich anzusprechen.`)
+      )
+    );
+  }
 }
 
 class Search {
-    view(vnode){
-        return section.section.fade(
-            h1.title('Suche nach Baustoffen'),
-            p.text('')
-        )
-    }
+  view(vnode) {
+    return section.section.fade(h1.title("Suche nach Baustoffen"), p.text(""));
+  }
 }
 
-
-var links = [{
-    link: '/',
-    text: 'Baustoff',
+var links = [
+  {
+    link: "/",
+    text: "Baustoff",
     component: Home
-},
-{
-    link: '/suche',
-    text: 'Suche',
+  },
+  {
+    link: "/suche",
+    text: "Suche",
     component: Search
-}, {
-    link: '/meine',
-    text: 'Meine Baustoffe',
+  },
+  {
+    link: "/meine",
+    text: "Meine Baustoffe",
     component: ExamplePage
-}
+  }
 ];
 
 class Navbar {
-    view(vnode) {
-        return nav.navbar.isInfo(            
-            div.navbarBrand(
-                links.map(link=>
-                    a.navbarItem({
-                        href:'#!'+link.link
-                    },link.text)
-                    )
+  view(vnode) {
+    return nav.navbar.isInfo(
+      div.navbarBrand(
+        links.map(link =>
+          a.navbarItem(
+            {
+              href: "#!" + link.link
+            },
+            link.text
+          )
+        )
 
-//                a.navbarItem('Home')
-            )
-        );
-    }
+        //                a.navbarItem('Home')
+      )
+    );
+  }
 }
 
 class Router {
-    oncreate(vnode) {
-        m.route(vnode.dom, '/queens',
-            links.reduce((acc, {
-                link,
-                component
-            }) => {
-                acc[link] = component;
-                return acc;
-            }, {})
-        );
-    }
-    view(vnode) {
-        return m('');
-    }
+  oncreate(vnode) {
+    m.route(
+      vnode.dom,
+      "/",
+      links.reduce((acc, { link, component }) => {
+        acc[link] = component;
+        return acc;
+      }, {})
+    );
+  }
+  view(vnode) {
+    return m("");
+  }
 }
 class Footer {
-    view(vnode){
-        return footer.footer(
-            div.content.hasTextCentered(
-                p(
-                    strong('Baustoff') ,' by ',
-                    a({href:"eismaenners.de"},'Andreas Eismann')
-                )
-            )
-        );
-    }
+  view(vnode) {
+    return footer.footer(
+      div.content.hasTextCentered(
+        p(
+          strong("Baustoff"),
+          " by ",
+          a({ href: "http://eismaenners.de" }, "Andreas Eismann")
+        )
+      )
+    );
+  }
 }
 
 class Layout {
-    view(vnode) {
-        return [
-            m(Navbar),
-            m(Router),
-            m(Footer)
-        ];
-    }
+  view(vnode) {
+    return [m(Navbar), m(Router), m(Footer)];
+  }
 }
 
+m.mount(document.getElementById("app"), {
+  view(vnode) {
+    return m(Layout);
+  }
+});
 
-m.mount(document.body,{
-    view(vnode){
-        return m(Layout);
-    }
-})
+carousel.attach();
